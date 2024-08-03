3bcd38425dd0800227e65f52b7db44447cb11cde
Ibrahim Ciloglu <ibrahimciloglu.ic@gmail.com>
2024-08-03 10:29:35 +0300

    add references page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,50 +1,56 @@
 'use client';
-
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
-import Link from 'next/link';
+import { motion, useScroll, useTransform, useInView } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRef } from 'react';
 
-const projects = [
+const items = [
   {
     id: '1',
-    color: 'from-blue-300 to violet-300',
-    title: 'Test',
-    desc: 'Lorem ipsum dolor sit amet conasd sdfiuh sdfigusdfb',
-    img: 'https://images.pexel....',
-    link: 'https://...',
+    color: 'white',
+    title: 'Rambler',
+    desc: 'Beta version of a sight seeing adventure app',
+    img: '/rambler-app.png',
+    link: 'https://rambler-app.netlify.app/',
   },
   {
     id: '2',
-    color: 'from-blue-300 to violet-300',
-    title: 'Test2',
-    desc: 'Lorem ipsum dolor sit amet conasd sdfiuh sdfigusdfb',
-    img: 'https://images.pexel....',
-    link: 'https://...',
+    color: 'white',
+    title: 'cleanapps beta website',
+    desc: 'Beta version of a startup company website',
+    img: '/cleanapps-beta.png',
+    link: 'https://cleanapps-beta.netlify.app/',
   },
   {
     id: '3',
-    color: 'from-blue-300 to violet-300',
-    title: 'Test3',
-    desc: 'Lorem ipsum dolor sit amet conasd sdfiuh sdfigusdfb',
-    img: 'https://images.pexel....',
-    link: 'https://...',
+    color: 'white',
+    title: 'password generator',
+    desc: 'Classic password generator',
+    img: '/pw-gen.png',
+    link: 'https://pw-gen-classic.netlify.app/',
   },
   {
     id: '4',
-    color: 'from-blue-300 to violet-300',
-    title: 'Test4',
-    desc: 'Lorem ipsum dolor sit amet conasd sdfiuh sdfigusdfb',
-    img: 'https://images.pexel....',
-    link: 'https://...',
+    color: 'white',
+    title: 'hangman',
+    desc: 'A simple hangman game with certain development words',
+    img: '/hangman.png',
+    link: 'https://hangman-23.netlify.app/',
   },
 ];
 
 const PortfolioPage = () => {
-  const ref = useRef();
+  const containerRef = useRef();
 
-  const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+  const { scrollYProgress } = useScroll({ container: containerRef });
+  const x = useTransform(scrollYProgress, [0, 1], ['0%', '-80%']);
+
+  const skillRef = useRef();
+  const isSkillRefInView = useInView(skillRef, { margin: '-100px' });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: '-100px' });
+
   return (
     <motion.div
       className="h-full"
@@ -52,28 +58,66 @@ const PortfolioPage = () => {
       animate={{ y: '0%' }}
       transition={{ duration: 1 }}
     >
-      <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center text-white">
-          My recent Projects
-        </div>
-        <div className="sticky top-0 flex h-screen gap-4 items-center">
-          {projects.map((project) => (
-            <div
-              className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${project.color}`}
-              key={project.id}
+      <div
+        id="container"
+        className="h-full overflow-scroll lg:flex"
+        ref={containerRef}
+      >
+        <div className="h-[600vh] relative" ref={containerRef}>
+          <div className="w-screen h-[calc(100vh-6rem)] flex flex-col items-center justify-center text-8xl text-center">
+            <h1 className="text-8xl text-center">My recent Projects</h1>
+            {/* BIOGRAPHY SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: '10px' }}
+              transition={{ repeat: Infinity, duration: 3, ease: 'easeInOut' }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
             >
-              <div className="flex flex-col gap-8 text-white">
-                <h1>{project.title}</h1>
-                <div className="relative">
-                  <Image src="" alt="" fill />
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#FFFFFF"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#FFFFFF" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#FFFFFF"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
+          <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+            <motion.div style={{ x }} className="flex">
+              <div className="h-screen w-screen flex items-center justify-center " />
+              {items.map((item) => (
+                <div
+                  className={`h-screen w-screen flex items-center justify-center`}
+                  key={item.id}
+                >
+                  <div className="flex flex-col gap-8 text-white">
+                    <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                      {item.title}
+                    </h1>
+                    <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                      <Image src={item.img} alt="" fill />
+                    </div>
+                    <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                      {item.desc}
+                    </p>
+                    <Link href={item.link} className="flex justify-end">
+                      <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                        See Demo
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-                <p>{project.desc}</p>
-                <Link href={project.link}>
-                  <button>See Demo</button>
-                </Link>
-              </div>
-            </div>
-          ))}
+              ))}
+            </motion.div>
+          </div>
         </div>
       </div>
     </motion.div>
